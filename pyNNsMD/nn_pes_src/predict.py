b303977beebb899ed7589b21999c7ddddc3a67c5
aimat <68069513+aimat@users.noreply.github.com>
2020-10-29 14:54:54 +0100

    Add files via upload

--- pyNNsMD/nn_pes_src/predict.py
+++ pyNNsMD/nn_pes_src/predict.py
@@ -9,37 +9,23 @@ import tensorflow as tf
 
 
 
+from pyNNsMD.nn_pes_src.predicting.predict_mlp_nac import _predict_uncertainty_mlp_nac
+from pyNNsMD.nn_pes_src.predicting.predict_mlp_eg import _predict_uncertainty_mlp_eg
 
 
 def _predict_uncertainty(model_type,out):
-    out_mean = []
-    out_std = []
     if(model_type == 'mlp_nac'):
-        out_mean = np.mean(np.array(out),axis=0)
-        out_std = np.std(np.array(out),axis=0,ddof=1)
+        return _predict_uncertainty_mlp_nac(out)
     elif(model_type == 'mlp_eg'):
-        for i in range(2):
-            out_mean.append(np.mean(np.array([x[i] for x in out]),axis=0))
-            out_std.append(np.std(np.array([x[i] for x in out]),axis=0,ddof=1))
+        return _predict_uncertainty_mlp_eg(out)
     else:
         print("Error: Unknown model type for predict",model_type)
         raise TypeError(f"Error: Unknown model type for predict {model_type}")
-        
-    return out_mean,out_std
 
 
+       
 
-def _call_convert_output_tonumpy(model_type,temp):
-    if(model_type == 'mlp_nac'):
-        return temp.numpy()
-    elif(model_type == 'mlp_eg'):
-        return [temp[0].numpy(),temp[1].numpy()]
-    else:
-        print("Error: Unknown model type for predict",model_type)
-        raise TypeError(f"Error: Unknown model type for predict {model_type}")
-        
-
-def _call_convert_x_totensor(model_type,x):
+def _call_convert_x_to_tensor(model_type,x):
     if(model_type == 'mlp_eg'):
         return tf.convert_to_tensor( x,dtype=tf.float32)
     elif(model_type == 'mlp_nac'):
@@ -48,3 +34,14 @@ def _call_convert_x_totensor(model_type,x):
         print("Error: Unknown model type for predict",model_type)
         raise TypeError(f"Error: Unknown model type for predict {model_type}")
     
+    
+    
+def _call_convert_y_to_numpy(model_type,temp):
+    if(model_type == 'mlp_nac'):
+        return temp.numpy()
+    elif(model_type == 'mlp_eg'):
+        return [temp[0].numpy(),temp[1].numpy()]
+    else:
+        print("Error: Unknown model type for predict",model_type)
+        raise TypeError(f"Error: Unknown model type for predict {model_type}")
+        
