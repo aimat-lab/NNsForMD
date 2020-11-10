0d4f137096d855d0d2420a98ef3bb09b39f3fadf
aimat <68069513+aimat@users.noreply.github.com>
2020-11-10 15:50:06 +0100

    Add files via upload

--- pyNNsMD/nn_pes_src/keras_utils/loss.py
+++ pyNNsMD/nn_pes_src/keras_utils/loss.py
@@ -7,8 +7,34 @@ Also includes Metrics and tools around loss.
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as ks
-    
 
+
+
+class ScaledMeanAbsoluteError(tf.keras.metrics.MeanAbsoluteError):
+
+    def __init__(self, scaling_shape = (), name='scaled_mean_absolute_error', **kwargs):
+        super(ScaledMeanAbsoluteError, self).__init__(name=name, **kwargs)
+        self.scale = self.add_weight(shape=scaling_shape,initializer=tf.keras.initializers.Ones() ,name='scale',dtype =tf.keras.backend.floatx())
+        self.scaling_shape = scaling_shape
+        
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = self.scale*y_true
+        y_pred = self.scale*y_pred
+        return super(ScaledMeanAbsoluteError, self).update_state(y_true, y_pred, sample_weight=sample_weight)
+
+
+    def reset_states(self):
+        #Super variables
+        ks.backend.set_value(self.total,0)
+        ks.backend.set_value(self.count,0)
+
+    def get_config(self):
+        """Returns the serializable config of the metric."""
+        mae_conf = super(ScaledMeanAbsoluteError, self).get_config()
+        mae_conf.update({"scaling_shape" : scaling_shape})
+        return mae_conf
+    
+    
 def get_lr_metric(optimizer):
     """
     Obtian learning rate from optimizer.
@@ -118,8 +144,12 @@ class NACphaselessLoss(ks.losses.Loss):
         phase_combo = tf.cast(self.phase_combo, y_pred.dtype)
         se = ks.backend.square(ks.backend.expand_dims(y_true,axis=0) - phase_combo*ks.backend.expand_dims(y_pred,axis=0) )
         mse = ks.backend.mean(se,axis=self.reduce_mean)
-        #Maybe we can add a softmin to improve convergence later.
-        return ks.backend.min(mse,axis=0)
+        #Softmin
+        #softmin = ks.backend.exp(-mse)
+        #softmin = softmin/ks.backend.sum(softmin)
+        #out = ks.backend.sum(softmin*mse)
+        out =  ks.backend.min(mse,axis=0)
+        return out
     
     def get_config(self):
         """Return the config dictionary for a `Loss` instance."""
