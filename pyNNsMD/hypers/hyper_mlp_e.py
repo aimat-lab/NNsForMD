4af92fd1451b707d32cc7a9503871b12878acd56
cm5828 <patrick.reiser@kit.edu>
2022-01-26 18:10:16 +0100

    Major refactoring for version 2.0

--- pyNNsMD/hypers/hyper_mlp_e.py
+++ pyNNsMD/hypers/hyper_mlp_e.py
@@ -33,7 +33,6 @@ DEFAULT_HYPER_PARAM_ENERGY = {
     },
     'training': {
         'initialize_weights': True,
-        # Keras fit parameters
         'epo': 3000,  # total epochs
         'batch_size': 64,  # batch size
         'epostep': 10,  # steps of epochs for validation, also steps for changing callbacks
@@ -52,21 +51,10 @@ DEFAULT_HYPER_PARAM_ENERGY = {
         'initialize_weights': False,
         'loss_weights': [1, 10],  # weights between energy and gradients
         'learning_rate': 1e-3,  # learning rate, can be modified by callbacks
-        # Whether to reset the weights before fit, used for retraining, transfer learning
-        'val_disjoint': True,  # Should be removed as always a disjoint validation split per instance is favourable
-        'val_split': 0.1,
         'epo': 1000,  # total epochs
         'batch_size': 64,  # batch size
         'epostep': 10,  # steps of epochs for validation, also steps for changing callbacks
-        # Callbacks
-        'step_callback': {'use': False, 'epoch_step_reduction': [500, 1500, 500, 500],
-                      'learning_rate_step': [1e-3, 1e-4, 1e-5, 1e-6]},
-        'linear_callback': {'use': False, 'learning_rate_start': 1e-3, 'learning_rate_stop': 1e-6, 'epomin': 100,
-                        'epo': 1000},
-        'early_callback': {'use': False, 'epomin': 5000, 'patience': 600, 'max_time': 600, 'delta_loss': 1e-5,
-                       'loss_monitor': 'val_loss', 'factor_lr': 0.1, 'learning_rate_start': 1e-3,
-                       'learning_rate_stop': 1e-6, 'epostep': 1},
-        'exp_callback': {'use': False, 'factor_lr': 0.1, 'epomin': 100, 'learning_rate_start': 1e-3},
+        "callbacks": [],
         'unit_energy': "eV",
         'unit_gradient': "eV/A"
     }
