819b8415ccc38e3db98dd3ddc165bb10af910b23
cm5828 <patrick.reiser@kit.edu>
2022-01-27 19:53:36 +0100

    Refactoring for version 2.0

--- pyNNsMD/scaler/nac.py
+++ pyNNsMD/scaler/nac.py
@@ -8,12 +8,14 @@ class NACStandardScaler:
                  use_x_std=False,
                  use_x_mean=False,
                  use_nac_std=True,
-                 use_nac_mean=True
+                 use_nac_mean=True,
+                 scaler_module="nac"
                  ):
         self.use_x_std = use_x_std
         self.use_x_mean = use_x_mean
         self.use_nac_std = use_nac_std
         self.use_nac_mean = use_nac_mean
+        self.scaler_module = scaler_module
 
         # Weights
         self.x_mean = np.zeros((1, 1, 1))
@@ -44,11 +46,11 @@ class NACStandardScaler:
 
     def fit(self, x, y):
         npeps = np.finfo(float).eps
-        if auto_scale['x_mean']:
+        if self.use_x_mean:
             self.x_mean = np.mean(x)
-        if auto_scale['x_std']:
+        if self.use_x_std:
             self.x_std = np.std(x) + npeps
-        if auto_scale['nac_std']:
+        if self.use_nac_std:
             self.nac_std = np.std(y, axis=(0, 3), keepdims=True) + npeps
             self.nac_mean = np.zeros_like(self.nac_std)
 
@@ -59,32 +61,31 @@ class NACStandardScaler:
         self.fit(x=x, y=y)
         return self.transform(x=x,y=y)
 
-    def save(self, filepath):
-        outdict = {
-            'x_mean': self.x_mean.tolist(),
-            'x_std': self.x_std.tolist(),
-            'nac_mean': self.nac_mean.tolist(),
-            'nac_std': self.nac_std.tolist()
+    def save_weights(self, file_path):
+        out_dict = {
+            'x_mean': self.x_mean,
+            'x_std': self.x_std,
+            'nac_mean': self.nac_mean,
+            'nac_std': self.nac_std
         }
-        with open(filepath, 'w') as f:
-            json.dump(outdict, f)
+        np.save(file_path, out_dict)
 
-    def load(self, filepath):
-        with open(filepath, 'r') as f:
-            indict = json.load(f)
-
+    def load_weights(self, file_path):
+        indict = np.load(file_path, allow_pickle=True).item()
         self.x_mean = np.array(indict['x_mean'])
         self.x_std = np.array(indict['x_std'])
         self.nac_mean = np.array(indict['nac_mean'])
         self.nac_std = np.array(indict['nac_std'])
 
     def get_params(self):
-        outdict = {'x_mean': self.x_mean.tolist(),
-                   'x_std': self.x_std.tolist(),
-                   'nac_mean': self.nac_mean.tolist(),
-                   'nac_std': self.nac_std.tolist(),
-                   }
-        return outdict
+        conf = {
+            'scaler_module': self.scaler_module,
+            'use_x_std': self.use_x_std,
+            'use_x_mean': self.use_x_mean,
+            'use_nac_mean': self.use_nac_mean,
+            'use_nac_std': self.use_nac_std,
+        }
+        return conf
 
     def print_params_info(self):
         print("Info: All-data NAC std", self._encountered_y_shape, ":", self._encountered_y_std[0, :, :, 0])
